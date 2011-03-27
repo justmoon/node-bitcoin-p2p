3f6c91292e6e335f53607dbf661abf70ff8e8fe3
Stefan Thomas <justmoon@members.fsf.org>
2011-03-28 00:57:50 +0100

    Added a first test - Bitcoin address en-/decoding.

---
+++ test/util.js
@@ -0,0 +1,33 @@
+var path = require('path');
+require.paths.unshift(path.join(__dirname, '..'));
+
+var vows = require('vows'),
+    assert = require('assert');
+
+var Util = require('lib/util');
+
+var suite = vows.describe('util');
+
+suite.addBatch({
+	'Bitcoin addresses': {
+		'decode a bitcoin address': {
+			topic: "12c6DSiU4Rq3P4ZxziKxzrL5LmMBrzjrJX",
+
+			'and check the result': function (topic) {
+				var addrHash = Util.addressToPubKeyHash(topic);
+				console.log(addrHash.toHex());
+				assert.equal(Util.pubKeyHashToAddress(addrHash), topic);
+			}
+		},
+		'encode a bitcoin address': {
+			topic: new Buffer("119b098e2e980a229e139a9ed01a469e518e6f26", 'hex'),
+
+			'and check the result': function (topic) {
+				var addrHash = Util.pubKeyHashToAddress(topic);
+				assert.equal(Util.addressToPubKeyHash(addrHash).compare(topic), 0);
+			}
+		}
+	}
+});
+
+suite.run();
