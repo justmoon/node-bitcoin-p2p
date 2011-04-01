ae215003bc8944ea3a91c43b342677b504adb016
Stefan Thomas <justmoon@members.fsf.org>
2011-04-01 19:21:59 +0100

    Created separate class for configuration.

--- examples/simple.js
+++ examples/simple.js
@@ -1,15 +1,5 @@
 var Bitcoin = require('../lib/bitcoin');
-var Storage = Bitcoin.Storage;
-var Node = Bitcoin.Node;
 
-var static_peers = ['localhost'];
-
-var storage = new Storage('mongodb://localhost/bitcoin');
-
-node = new Node(storage);
-
-for (var i in static_peers) {
-	node.addPeer(static_peers[i]);
-}
-
+node = new Bitcoin.Node();
+node.addPeer('localhost');
 node.start();
